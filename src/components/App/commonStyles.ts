017186aa5a67b1d2e080a7b25a0a1d43be7cc016
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-02 20:18:33 -0300

    Add form

--- src/components/App/commonStyles.ts
+++ src/components/App/commonStyles.ts
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+export const Title = styled.h2`
+  margin-bottom: 2rem;
+`;
+
+export const PageContainer = styled.div`
+  margin-top: 3rem;
+`;
+
 export const Wrapper = styled.div`
   max-width: 1200px;
   width: 90%;
--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,5 +1,100 @@
 import { FunctionComponent } from "react";
+import styled from "styled-components";
+import { Button, Input } from "@/components";
+import { PageContainer, Wrapper, Title } from "@/components/App";
+import { useForm } from "@/hooks";
+
+type Fields = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export const Register: FunctionComponent = () => {
-  return <div>Register</div>;
+  const { fields, errors, handleInputChange, handleSubmit } = useForm<Fields>({
+    intialValues: { email: "", password: "", confirmPassword: "" },
+    validations: {
+      email: {
+        required: {
+          value: true,
+          message: "Campo requerido",
+        },
+        custom: {
+          isValid: (value) => value.includes("@"),
+          message: "Mail invalido",
+        },
+      },
+      password: {
+        required: {
+          value: true,
+          message: "Campo requerido",
+        },
+      },
+      confirmPassword: {
+        required: {
+          value: true,
+          message: "Campo requerido",
+        },
+      },
+    },
+  });
+
+  const onSubmit = () => {
+    console.log(fields);
+  };
+
+  return (
+    <PageContainer>
+      <Wrapper>
+        <Title>Register</Title>
+        <form onSubmit={(e) => handleSubmit(e, onSubmit)}>
+          <InputWrapper>
+            <Input
+              placeholder="Email"
+              value={fields.email}
+              error={errors?.email}
+              name="email"
+              onChange={handleInputChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="password"
+              placeholder="Contraseña"
+              value={fields.password}
+              error={errors?.password}
+              name="password"
+              onChange={handleInputChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="password"
+              placeholder="Confirmar contraseña"
+              value={fields.confirmPassword}
+              error={errors?.confirmPassword}
+              name="confirmPassword"
+              onChange={handleInputChange}
+            />
+          </InputWrapper>
+          <ButtonWrapper>
+            <Button block type="submit">
+              Registarse
+            </Button>
+          </ButtonWrapper>
+        </form>
+      </Wrapper>
+    </PageContainer>
+  );
 };
+
+const InputWrapper = styled.div`
+  margin-bottom: 1rem;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+`;
