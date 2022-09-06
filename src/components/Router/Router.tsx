fb61c23fadd1eeba21b772954334238ea017da6d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-06 17:03:41 -0300

    Remove console log

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -2,13 +2,8 @@ import { FunctionComponent } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "..";
 import { Home, Login, Register } from "@/containers";
-import { useSession } from "@/contexts";
 
 export const Router: FunctionComponent = () => {
-  const { isAuth } = useSession();
-
-  console.log({ isAuth });
-
   return (
     <BrowserRouter>
       <Layout>
