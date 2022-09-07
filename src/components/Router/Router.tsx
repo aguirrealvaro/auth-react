a62831ebd0975e9aeb0dfbf564d9076e1a773ca7
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-09-07 14:00:22 -0300

    Add 404 not found page

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,14 +1,15 @@
 import { FunctionComponent } from "react";
 import { Routes, Route } from "react-router-dom";
 import { PrivateRoute } from "../PrivateRoute";
-import { Home, Login, Register } from "@/containers";
+import { Home, Login, NotFound, Register } from "@/containers";
 
 export const Router: FunctionComponent = () => {
   return (
     <Routes>
+      <Route path="/" element={<PrivateRoute component={Home} />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<PrivateRoute component={Home} />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/containers/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+import { PageContainer, Wrapper } from "@/components/App";
+
+export const NotFound: FunctionComponent = () => {
+  return (
+    <PageContainer>
+      <Wrapper>404 Not found</Wrapper>
+    </PageContainer>
+  );
+};
